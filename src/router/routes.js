88d976fa53582bdf84d5a56446d4a25173fb4f52
Cy-Bert <125924936+Cy-Bert@users.noreply.github.com>
2023-09-27 13:45:39 +0200

    WIP

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,3 @@
-// const routes = [
-//   {
-//     path: "/",
-//     component: () => import("layouts/MainLayout.vue"),
-//     children: [
-//       { path: "", component: () => import("pages/IndexPage.vue") },
-
-//       {
-//         path: "/dashboard",
-
-//         component: () => import("pages/Dashboard.vue"),
-//       },
-//     ],
-//   },
-//   {
-//     path: "/:catchAll(.*)*",
-//     component: () => import("pages/ErrorNotFound.vue"),
-//   },
-// ];
-
-// export default routes;
-
    import coreRoutes from './coreRoutes'
 
 
